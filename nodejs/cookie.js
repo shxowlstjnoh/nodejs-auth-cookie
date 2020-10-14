a411d1a45330a665b77f6cb14d41d3cf98879bab
taejin_noh <taejin_noh@tmax.co.kr>
2020-10-14 18:07:16 +0900

    쿠키 활용 및 옵션

---
+++ nodejs/cookie.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var cookie = require('cookie');
+
+http.createServer(function (request, response) {
+    console.log(request.headers.cookie);
+    var cookies = {};
+    if (request.headers.cookie !== undefined) {
+        cookies = cookie.parse(request.headers.cookie);
+    }
+    console.log(cookies);
+    response.writeHead(200, {
+        'Set-Cookie': [
+            'yummy_cookie=choco',
+            'tasty_cookie=strawberry',
+            `Permanet=cookies; Max-Age=${60 * 60 * 24 * 30}`,
+            'Secure=Secure; Secure',
+            'HttpOnly=HttpOnly; HttpOnly',
+        ],
+    });
+    response.end('Cookie');
+}).listen(3000);
